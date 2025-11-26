d2f93fcc7416ffd2f13573a6d54e50fcc8f534ed
Lobo <gui.wolflobo@gmail.com>
2025-11-26 20:44:02 -0300

    Add initial plugin setup files, including `.idea` configurations and base plugin boilerplate.

---
+++ categoryPromotionWooPlugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Category Promotions for WooCommerce
+ * Description: Simple base plugin – adds a menu under WooCommerce > Marketing.
+ * Version: 1.0.0
+ * Author: Guilherme Lobo – Indoor Tech
+ * Author URI: https://indoortech.com.br
+ * Text Domain: indoortech-category-promotions
+ * Domain Path: /languages
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
